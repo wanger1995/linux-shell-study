7dcab0163d29e5c1de7c946767f70d17ee7253c4
unknown <wanger1995@163.com>
2018-08-17 17:08:02 +0800

    更改微服务旧tag获取，增加update.log

--- Mss-update.sh
+++ Mss-update.sh
@@ -4,19 +4,21 @@ function run()
     MSS_NAME=$1 && \
     TAG=$2 && \
     cd ~/.docker-compose/wesign-enterprise && \
-    TAG_OLD=`cat wesign-mss-certificate-app.yml |grep image | awk -F ":" '{printf $4}' |awk -F '"' '{printf $1}'` && \
+    TAG_OLD=`cat wesign-mss-${MSS_NAME}-app.yml |grep image | awk -F ":" '{printf $4}' |awk -F '"' '{printf $1}'` && \
     
 
     sed -i "s/${TAG_OLD}/${TAG}/g" wesign-mss-${MSS_NAME}-app.yml && \
-    docker stop `docker ps | grep $MSS_NAME |awk '{printf $1}'` >/dev/null 2>&1
-    docker-compose -f wesign-mss-${MSS_NAME}-app.yml up -d >/dev/null 2>&1
+    docker stop `docker ps | grep ${MSS_NAME} |awk '{printf $1}'` >/dev/null 2>&1
+    docker-compose -f wesign-mss-${MSS_NAME}-app.yml up -d >>update.log
     if [ $? -eq 0 ] 
     then
-        echo "+++++++++++++ ${MSS_NAME} updated successful +++++++++++++++"
+        echo -e "\t\033[32;49;1m ${MSS_NAME} updated successful  \033[39;49;0m"
     else 
-        echo "+++++++++++++ ${MSS_NAME} updated ERROR ++++++++++++++++++++"
+        echo -e "\t\033[31;49;1m ${MSS_NAME} updated faild,cat update.log  \033[39;49;0m"
     fi
 }
+
+
 
 run api 1.2.1.RELEASE && \
 run developer 1.0.3.RELEASE && \
